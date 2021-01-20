3838c0b5992fe09a91e84c13d28c33663eaeb11a
FundWit <fundwit@qq.com>
2021-01-21 00:56:39 +0800

    run with a test mysql instance an default user and group.

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"flywheel/persistence"
 	"flywheel/security"
 	"flywheel/servehttp"
+	"flywheel/testinfra"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -19,6 +21,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("parse database config failed %v\n", err)
 	}
+
+	testDatabase := testinfra.StartMysqlTestDatabase("flywheel")
+	err = testDatabase.DS.GormDB().AutoMigrate(&domain.Work{}, &flow.WorkStateTransition{}).Error
+	if err != nil {
+		log.Fatalf("database migration failed %v\n", err)
+	}
+	defer testinfra.StopMysqlTestDatabase(testDatabase)
+	dbConfig = testDatabase.DS.DatabaseConfig
 
 	// create database (no conflict)
 	if dbConfig.DriverType == "mysql" {
@@ -39,6 +49,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("database migration failed %v\n", err)
 	}
+
+	ds.GormDB().Create(&security.User{ID: 1, Name: "admin", Secret: security.HashSha256("123456")})
+	ds.GormDB().Create(&domain.Group{ID: 1, Name: "default", CreateTime: time.Now(), Creator: 1})
+	ds.GormDB().Create(&domain.GroupMember{MemberId: 1, GroupID: 1, Role: "owner", CreateTime: time.Now()})
 
 	engine := gin.Default()
 	engine.Use(servehttp.ErrorHandling())
